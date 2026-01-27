1139424c7a29b59690fe80f06bf8723c227146a4
Ahmed_Abouelwafa <hamada.abouelwafa12@gmail.com>
2026-01-27 11:08:27 +0200

    add

--- TargetSocialApp.Application/obj/Debug/net9.0/TargetSocialApp.Application.AssemblyInfo.cs
+++ TargetSocialApp.Application/obj/Debug/net9.0/TargetSocialApp.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TargetSocialApp.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd86e08bd44b733cc7614391b82891ed55ec21f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a61064528cf2989800e98c0ded3e7a97df1aeef9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TargetSocialApp.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TargetSocialApp.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
